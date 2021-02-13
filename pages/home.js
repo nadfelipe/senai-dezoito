829230441f79beccf7459796c2e97276f680b4c9
nadfelipe <nadfelipe.s@gmail.com>
2021-02-13 19:47:33 -0300

    page-progress ok

--- pages/home.js
+++ pages/home.js
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css';
 import Container from '../src/components/Container';
 import ArrowScroll from '../src/components/ArrowDown'
 import styled from 'styled-components';
+import ReadingProgress from '../src/components/PageProgress'
 
 const Banner = styled.section`
     width: 80vw;
@@ -62,20 +63,72 @@ const Section3 = styled.section`
     background: yellowgreen;
 `;
 
+const DivReadingProgress = styled.div`
+    progress {
+        height: 20px;
+        color: #5D3C74 !important;
+        border: 1 !important;
+    }
+`;
+
+// const ProgressBar = styled.div`
+//   position: fixed;
+//   height: 5px;
+//   top: 0;
+//   background-color: #ff0000;
+// `;
+
+// const ReadingProgress = ({target}) => {
+//     const [readingProgress, setReadingProgress] = useState(0);
+//     console.log(target)
+//     const scrollListener = () => {
+//       if (!target.current) {
+//         return;
+//       }
+  
+//       const element         = target.current;
+//       const totalHeight     = element.clientHeight - element.offsetTop - (window.innerHeight);
+//       const windowScrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  
+//       if (windowScrollTop === 0) {
+//         return setReadingProgress(0);
+//       }
+  
+//       if (windowScrollTop > totalHeight) {
+//         return setReadingProgress(100);
+//       }
+
+//       setReadingProgress((windowScrollTop / totalHeight) * 100);
+//     };
+    
+//     useEffect(() => {
+//       window.addEventListener("scroll", scrollListener);
+//       return () => window.removeEventListener("scroll", scrollListener);
+//     });
+    
+//     console.log(readingProgress)
+
+//     return <ProgressBar style={{width: `${readingProgress}%`}} />;
+//   };
+
 
 export default function HomePage(){
+    const ref = React.createRef();
     return(
-        <Container className={styles.homepage} >
-            <Banner>
-                <Banner.Textos>
-                    <Banner.H1>SENAI INFO</Banner.H1>
-                    <Banner.H2>#dezoitou</Banner.H2>
-                </Banner.Textos>
-                <ArrowScroll />
-            </Banner>
-            <Section1></Section1>
-            <Section2></Section2>
-            <Section3></Section3>
-        </Container>
+        <>
+            <ReadingProgress />
+            <Container className={styles.homepage} >
+                <Banner>
+                    <Banner.Textos>
+                        <Banner.H1>SENAI INFO</Banner.H1>
+                        <Banner.H2>#dezoitou</Banner.H2>
+                    </Banner.Textos>
+                    <ArrowScroll />
+                </Banner>
+                <Section1></Section1>
+                <Section2></Section2>
+                <Section3></Section3>
+            </Container>
+        </>
     )
 }
---
+++ src/components/PageProgress/index.js
@@ -0,0 +1,37 @@
+import React,  { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+const ReadingProgressWrapper = styled.div`
+    position: fixed;
+    height: 2px;
+    top: 0;
+    width: 100%;
+`;
+const ReadingProgressBar = styled.div`
+    width: ${props => props.width};
+    background-color: #E39600;
+    position: fixed;
+    z-index: 1;
+    height: 4px;
+`;
+
+const ReadingProgress = ({target}) => {
+    const [readingProgress, setReadingProgress] = useState(0);
+    const scrollListener = (target) => {
+        var scrollMaxY = window.scrollMaxY || (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+        setReadingProgress(`${Math.floor((window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop) / scrollMaxY * 100)}%`);
+        console.log(readingProgress)
+    };
+    
+    useEffect(() => {
+      window.addEventListener("scroll", scrollListener);
+      return () => window.removeEventListener("scroll", scrollListener);
+    });
+  
+    return (
+        <ReadingProgressWrapper>
+            <ReadingProgressBar width={readingProgress} />
+        </ReadingProgressWrapper>
+    );
+  };
+  export default ReadingProgress;
