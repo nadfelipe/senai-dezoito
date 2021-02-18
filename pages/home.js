df3515d9c5e561ec5f6b992cb08550700d5891bf
adminlocal <adminlocal@iMac-de-adminlocal-85.local>
2021-02-18 11:25:50 -0300

    up

--- pages/home.js
+++ pages/home.js
@@ -10,6 +10,36 @@ import ReadingProgress from '../src/components/PageProgress'
 
 import { Banner, Section1, Section2, Paragraph } from '../src/components/Home'
 
+const Gallery = styled.div`
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+Gallery.Content = styled.section`
+    max-width: 80%;
+    display: grid;
+    grid-gap: 10px;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: repeat(5, 580px) 800px repeat(4, 580px) 800px repeat(2, 580px) 800px;
+    grid-template-areas: 
+    "img1 img2"
+    "img1 img3"
+    "img4 img5"
+    "img6 img7"
+    "img8 img9"
+    "img10 img10"
+    "img11 img12"
+    "img13 img12"
+    "img13 img14"
+    "img15 img16"
+    "img17 img17"
+    "img18 img19"
+    "img20 img21"
+    "img22 img22";
+`;
+
 export default function HomePage(){
     return(
         <>
@@ -51,6 +81,32 @@ export default function HomePage(){
                         </Paragraph>
                     </Section2.Content>
                 </Section2>
+                <Gallery>
+                    <Gallery.Content>
+                        <div>1</div>
+                        <div>2</div>
+                        <div>3</div>
+                        <div>4</div>
+                        <div>5</div>
+                        <div>6</div>
+                        <div>7</div>
+                        <div>8</div>
+                        <div>9</div>
+                        <div>10</div>
+                        <div>11</div>
+                        <div>12</div>
+                        <div>13</div>
+                        <div>14</div>
+                        <div>15</div>
+                        <div>16</div>
+                        <div>17</div>
+                        <div>18</div>
+                        <div>19</div>
+                        <div>20</div>
+                        <div>21</div>
+                        <div>22</div>
+                    </Gallery.Content>
+                </Gallery>
             </Container>
         </>
     )
